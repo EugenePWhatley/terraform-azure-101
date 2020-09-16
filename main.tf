b44ea7d8a3af8a1f08f63266fb24fe416b998786
Eugene Whatley <ewhatley@thoughtworks.com>
2020-09-16 17:49:25 -0400

    Extract resource group information to variables

--- main.tf
+++ main.tf
@@ -4,6 +4,6 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "web_server_rg" {
-  name     = "web-rg"
-  location = "westus2"
+  name     = var.web_server_rg
+  location = var.web_server_location
 }
